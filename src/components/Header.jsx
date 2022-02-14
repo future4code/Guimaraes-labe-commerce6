efa20122f4be77a46ae70ea8427cf459b501c3e0
Thiago Vieira <thiago.cng@gmail.com>
2022-02-14 00:25:24 -0300

    adicionado styled-components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,38 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import { AppHeader } from "../styles";
+import logo from '../assets/rocket.png'
+import avatar from '../assets/avatar-img.png'
 
 export default function Header() {
   return (
-    <div className='header'>
+    <div>
+      <AppHeader>
         <div className="header-logo">
-            <img src="https://cdn-icons.flaticon.com/png/512/4710/premium/4710098.png?token=exp=1644804764~hmac=1c49a9279d449f50aec102795e23c2b9" id='rocket' alt="rocket" />
-            <h1>LabeCommerce</h1>
+          <img
+            src={logo}
+            id="rocket"
+            alt="rocket"
+          />
+          <h1>LabeCommerce</h1>
         </div>
         <div className="header-links">
-            <ul>
-                <li>Anuncie</li>
-                <li>Registre-se</li>
-            </ul>
+          <ul>
+            <li>Anuncie</li>
+            <li>Registre-se</li>
+          </ul>
         </div>
         <div className="header-buttons">
-            <button>Login</button>
-            <button>Meu Carrinho</button>
+          <button>Login</button>
+          <button>Meu Carrinho</button>
         </div>
         <div className="avatar">
-            <img src="https://cdn-icons.flaticon.com/png/512/3541/premium/3541871.png?token=exp=1644804951~hmac=6d9d25dc257298367dcd8e353fcf7618" id='avatar' alt="avatar" />
+          <img
+            src={avatar}
+            id="avatar"
+            alt="avatar"
+          />
         </div>
-
-
+      </AppHeader>
     </div>
-  )
+  );
 }
